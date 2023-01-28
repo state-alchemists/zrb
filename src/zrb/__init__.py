f9070f806e8f9777b0bd338cd2e17949e34f69c7
goFrendi <gofrendiasgard@gmail.com>
2023-01-29 00:49:57 +0700

    fix port checker location

--- src/zrb/__init__.py
+++ src/zrb/__init__.py
@@ -3,6 +3,5 @@ from .task.base_task import BaseTask
 from .task.task import Task
 from .task.cmd_task import CmdTask
 from .task.http_checker import HTTPChecker
-from .task.http_checker import PortChecker
+from .task.port_checker import PortChecker
 from .task_input.str_input import StrInput
-
