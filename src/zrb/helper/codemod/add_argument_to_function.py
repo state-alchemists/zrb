1a85293057a42f718fb77e527a8bfa8cfe1a05df
goFrendi <gofrendiasgard@gmail.com>
2023-04-13 20:50:24 +0700

    add codemod

---
+++ src/zrb/helper/codemod/add_argument_to_function.py
@@ -0,0 +1,36 @@
+import libcst as cst
+
+
+class AddArgumentTransformer(cst.CSTTransformer):
+    def __init__(
+        self, function_name: str, argument_name: str, argument_type: str
+    ):
+        self.function_name = function_name
+        self.argument_name = argument_name
+        self.argument_type = argument_type
+        super().__init__()
+
+    def leave_FunctionDef(
+        self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef
+    ) -> cst.FunctionDef:
+        if updated_node.name.value == self.function_name:
+            new_params = updated_node.params.params + (
+                cst.Param(
+                    cst.Name(self.argument_name),
+                    cst.Annotation(cst.Name(self.argument_type)),
+                ),
+            )
+            return updated_node.with_changes(
+                params=cst.Parameters(new_params),
+            )
+        return updated_node
+
+
+def add_argument_to_function(
+    code: str, function_name: str, argument_name: str, argument_type: str
+) -> str:
+    module = cst.parse_module(code)
+    transformed_module = module.visit(
+        AddArgumentTransformer(function_name, argument_name, argument_type)
+    )
+    return transformed_module.code
---
+++ src/zrb/helper/codemod/add_argument_to_function_call.py
@@ -0,0 +1,31 @@
+import libcst as cst
+
+
+class AddArgumentTransformer(cst.CSTTransformer):
+    def __init__(self, function_name: str, argument_name: str):
+        self.function_name = function_name
+        self.argument_name = argument_name
+        super().__init__()
+
+    def leave_Call(
+        self, original_node: cst.Call, updated_node: cst.Call
+    ) -> cst.Call:
+        if (
+            isinstance(updated_node.func, cst.Name)
+            and updated_node.func.value == self.function_name
+        ):
+            new_args = updated_node.args + (
+                cst.Arg(cst.Name(self.argument_name)),
+            )
+            return updated_node.with_changes(args=new_args)
+        return updated_node
+
+
+def add_argument_to_function_call(
+    code: str, function_name: str, argument_name: str
+) -> str:
+    module = cst.parse_module(code)
+    transformed_module = module.visit(
+        AddArgumentTransformer(function_name, argument_name)
+    )
+    return transformed_module.code
