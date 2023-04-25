8eed64b8a0c111ec705c412be4aa43ce857d1ddb
goFrendi <gofrendiasgard@gmail.com>
2023-04-25 09:28:18 +0700

    fix test

--- src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/test/auth/test_user_login.py
+++ src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/test/auth/test_user_login.py
@@ -32,6 +32,17 @@ async def test_create_normal_user_and_login_with_username(
     test_client_generator: AsyncIterator[AsyncClient]
 ):
     async for client in test_client_generator:
+        # login
+        login_admin_response = await client.post(
+            '/api/v1/login',
+            json={
+                'identity': app_auth_admin_username,
+                'password': app_auth_admin_password
+            }
+        )
+        assert login_admin_response.status_code == 200
+        admin_access_token = login_admin_response.json().get('access_token', '')
+        assert admin_access_token != ''
         # create user
         create_response = await client.post(
             '/api/v1/auth/users',
@@ -43,18 +54,21 @@ async def test_create_normal_user_and_login_with_username(
                 'groups': [],
                 'permissions': [],
                 'description': ''
+            },
+            headers={
+                'Authorization': 'Bearer ' + admin_access_token
             }
         )
         assert create_response.status_code == 200
         # login
-        login_response = await client.post(
+        login_admin_response = await client.post(
             '/api/v1/login',
             json={
                 'identity': 'test-login-user-success',
                 'password': 'test-password'
             }
         )
-        assert login_response.status_code == 200
-        json_login_response = login_response.json()
+        assert login_admin_response.status_code == 200
+        json_login_response = login_admin_response.json()
         assert json_login_response.get('token_type', '') == 'bearer'
         assert json_login_response.get('access_token', '') != ''
