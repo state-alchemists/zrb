bb7512ef59c09ccffc1aa9ad2b7a0c6e42c9f47c
goFrendi <gofrendiasgard@gmail.com>
2023-05-03 06:13:24 +0700

    add preprocessor

--- src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/src/frontend/svelte.config.js
+++ src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/src/frontend/svelte.config.js
@@ -1,6 +1,8 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
  
 export default {
+	preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
