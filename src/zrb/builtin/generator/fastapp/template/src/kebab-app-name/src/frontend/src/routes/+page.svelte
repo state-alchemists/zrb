ae17b9dbb0ec0ede035231226524084cd11e8647
goFrendi <gofrendiasgard@gmail.com>
2023-06-17 07:05:04 +0700

    update npm packages, edit front page

--- src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/src/frontend/src/routes/+page.svelte
+++ src/zrb/builtin/generator/fastapp/template/src/kebab-app-name/src/frontend/src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1 class="text-3xl font-bold underline">Welcome to PascalAppName</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>You can find this page source code on: <code>kebab-app-name/src/frontend/src/routes/+page.svelte</code></p>
+<p>To add navigation menu, you can visit: <code>kebab-app-name/src/frontend/src/lib/config/navData.ts</code></p>
+<p>Visit <a href="https://kit.svelte.dev/docs/project-structure">Sveltekit Documentation</a> for more information</p>
