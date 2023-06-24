a7e810fc2cab56a3a2c05710fa34ca135e257ac6
goFrendi <gofrendiasgard@gmail.com>
2023-06-24 17:18:25 +0700

    remove show command

--- src/zrb/builtin/env.py
+++ src/zrb/builtin/env.py
@@ -11,9 +11,9 @@ from ..runner import runner
 
 
 @python_task(
-    name='show',
+    name='get',
     group=env_group,
-    description='Show environment values',
+    description='Get environment values',
     runner=runner
 )
 async def show(*args: Any, **kwargs: Any):
@@ -27,14 +27,4 @@ async def show(*args: Any, **kwargs: Any):
         colored_name = colored(name, color='green', attrs=['bold'])
         colored_value = colored(value, attrs=['bold'])
         task.print_out(f'{colored_name}{colored_equal}{colored_value}')
-
-
-@python_task(
-    name='get',
-    group=env_group,
-    description='Get environment values',
-    runner=runner
-)
-async def get(*args: Any, **kwargs: Any) -> Mapping[str, str]:
-    task: Task = kwargs['_task']
-    return task.get_env_map()
+    return env_map
