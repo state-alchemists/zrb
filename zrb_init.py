2558b55e53aee8b9da685b61eac59f99fd60f475
goFrendi <gofrendiasgard@gmail.com>
2023-06-29 11:31:05 +0700

    change prompt to avoid confusion

--- zrb_init.py
+++ zrb_init.py
@@ -25,7 +25,7 @@ zrb_image_name_input = StrInput(
 zrb_latest_image_name_input = StrInput(
     name='zrb-latest-image-name',
     description='Zrb latest image name',
-    prompt='Zrb image name',
+    prompt='Zrb latest image name',
     default='docker.io/stalchmst/zrb:latest'
 )
 
