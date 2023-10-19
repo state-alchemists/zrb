0a0b2bc93731aa855a5c51ba1c40830338d4a34f
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-10-19 18:52:07 +0700

    update value

--- src/zrb/builtin/generator/fastapp/template/_automate/snake_zrb_app_name/_common.py
+++ src/zrb/builtin/generator/fastapp/template/_automate/snake_zrb_app_name/_common.py
@@ -31,8 +31,8 @@ MODULES = jsons.loads(MODULE_JSON_STR)
 
 
 def should_start_local_microservices(*args: Any, **kwargs: Any) -> bool:
-    if kwargs.get('local_snake_zrb_app_name', True):
-        return True
+    if not kwargs.get('local_snake_zrb_app_name', True):
+        return False
     run_mode = kwargs.get('snake_zrb_app_name_run_mode', 'monolith')
     return run_mode == 'microservices'
 
