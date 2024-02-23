9d5c79064a7e855267c498ca162cffe252fa17a5
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2024-02-23 21:44:06 +0700

    Update poetry (#98)
    * Update poetry
    
    * Poetry should be installed separately
    
    * update script
    
    * update script

--- src/zrb/builtin/generator/plugin/template/_cmd/activate-venv.sh
+++ src/zrb/builtin/generator/plugin/template/_cmd/activate-venv.sh
@@ -1,12 +1,17 @@
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+if ! command_exists poetry
+then
+    echo 'Install poetry'
+    pip install --upgrade pip setuptools
+    pip install "poetry==1.7.1"
+fi
 if [ ! -d .venv ]
 then
   echo "Init virtual environment"
   python -m venv .venv
-  source .venv/bin/activate
-  echo "Upgrade Pip"
-  pip install -U pip
-  echo "Install Poetry"
-  pip install "poetry==1.7.1"
 fi
 echo "Activate virtual environment"
 source .venv/bin/activate
