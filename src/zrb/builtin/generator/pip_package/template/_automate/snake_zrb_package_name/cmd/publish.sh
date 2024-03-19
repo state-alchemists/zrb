697177e80f965b041c7fb17e37c4341b9220ba18
gofrendi <gofrendiasgard@gmail.com>
2024-03-19 10:37:52 +0700

    update publish pip command

--- src/zrb/builtin/generator/pip_package/template/_automate/snake_zrb_package_name/cmd/publish.sh
+++ src/zrb/builtin/generator/pip_package/template/_automate/snake_zrb_package_name/cmd/publish.sh
@@ -1,2 +1,2 @@
 echo "Publish"
-poetry publish --repository {{input.snake_zrb_package_name_repo}}
+poetry publish
--- src/zrb/builtin/generator/plugin/template/_cmd/publish.sh
+++ src/zrb/builtin/generator/plugin/template/_cmd/publish.sh
@@ -1,2 +1,2 @@
 echo "Publish"
-poetry publish --repository {{input.plugin_repo}}
+poetry publish
