ece7fbeb3b352aa3ed95a317af8cfd5450c9c2d0
gofrendi <gofrendiasgard@gmail.com>
2024-03-30 21:30:33 +0700

    fix plugin

--- src/zrb/builtin/project/add/plugin/template/_automate/snake_zrb_package_name/prepare_venv/prepare_venv.py
+++ src/zrb/builtin/project/add/plugin/template/_automate/snake_zrb_package_name/prepare_venv/prepare_venv.py
@@ -4,17 +4,17 @@ from zrb import CmdTask, runner
 from .._group import kebab_zrb_package_name_group
 
 CURRENT_DIR = os.path.dirname(__file__)
-AUTOMATE_DIR = os.path.dirname(CURRENT_DIR)
-PROJECT_DIR = os.path.dirname(os.path.dirname(AUTOMATE_DIR))
-RESOURCE_DIR = os.path.join(PROJECT_DIR, "src", "kebab-zrb-package-name")
+PKG_AUTOMATE_DIR = os.path.dirname(CURRENT_DIR)
+PROJECT_DIR = os.path.dirname(os.path.dirname(PKG_AUTOMATE_DIR))
+PKG_RESOURCE_DIR = os.path.join(PROJECT_DIR, "src", "kebab-zrb-package-name")
 
 prepare_snake_zrb_package_name_venv = CmdTask(
     name="prepare-venv",
     description="Prepare human readable zrb package name venv",
     group=kebab_zrb_package_name_group,
-    cwd=RESOURCE_DIR,
+    cwd=PKG_RESOURCE_DIR,
     cmd_path=[
-        os.path.join(CURRENT_DIR, "activate-venv.sh"),
+        os.path.join(PKG_AUTOMATE_DIR, "activate-venv.sh"),
         os.path.join(CURRENT_DIR, "prepare-venv.sh"),
     ],
 )
--- src/zrb/builtin/project/add/plugin/template/_automate/snake_zrb_package_name/publish/publish.py
+++ src/zrb/builtin/project/add/plugin/template/_automate/snake_zrb_package_name/publish/publish.py
@@ -6,18 +6,18 @@ from .._group import kebab_zrb_package_name_group
 from ..build import build_snake_zrb_package_name
 
 CURRENT_DIR = os.path.dirname(__file__)
-AUTOMATE_DIR = os.path.dirname(CURRENT_DIR)
-PROJECT_DIR = os.path.dirname(os.path.dirname(AUTOMATE_DIR))
-RESOURCE_DIR = os.path.join(PROJECT_DIR, "src", "kebab-zrb-package-name")
+PKG_AUTOMATE_DIR = os.path.dirname(CURRENT_DIR)
+PROJECT_DIR = os.path.dirname(os.path.dirname(PKG_AUTOMATE_DIR))
+PKG_RESOURCE_DIR = os.path.join(PROJECT_DIR, "src", "kebab-zrb-package-name")
 
 publish_snake_zrb_package_name = CmdTask(
     name="publish",
     description="Publish human readable zrb package name",
     group=kebab_zrb_package_name_group,
     upstreams=[build_snake_zrb_package_name],
-    cwd=RESOURCE_DIR,
+    cwd=PKG_RESOURCE_DIR,
     cmd_path=[
-        os.path.join(AUTOMATE_DIR, "activate-venv.sh"),
+        os.path.join(PKG_AUTOMATE_DIR, "activate-venv.sh"),
         os.path.join(CURRENT_DIR, "publish.sh"),
     ],
 )
