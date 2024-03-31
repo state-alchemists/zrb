8db82596903aa0bb7f864bb1bf3070bfdcc7558d
gofrendi <gofrendiasgard@gmail.com>
2024-03-31 14:01:29 +0700

    remove app generator

--- src/zrb/builtin/generator/app_generator/template/base/_automate/generate_snake_zrb_meta_template_name/template/_automate/snake_zrb_app_name/image.py
+++
@@ -1,9 +0,0 @@
-from zrb import Env
-
-###############################################################################
-# 🌱 Env Definitions
-###############################################################################
-
-image_env = Env(
-    name="IMAGE", os_name="CONTAINER_ZRB_ENV_PREFIX_IMAGE", default="zrbMetaBaseImage"
-)
