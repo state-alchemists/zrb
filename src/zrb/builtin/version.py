96922e371d68d7e7a895ba20584cc285d63d616a
gofrendi <gofrendiasgard@gmail.com>
2024-04-11 20:42:13 +0700

    improve version command

--- src/zrb/builtin/version.py
+++ src/zrb/builtin/version.py
@@ -2,8 +2,11 @@ from zrb.config.config import version
 from zrb.helper.typing import Any
 from zrb.runner import runner
 from zrb.task.decorator import python_task
+from zrb.task.task import Task
 
 
 @python_task(name="version", description="Get Zrb version", runner=runner)
 async def get_version(*args: Any, **kwargs: Any) -> str:
+    task: Task = kwargs.get("_task")
+    task.print_out(version)
     return version
