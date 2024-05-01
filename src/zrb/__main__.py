70ab16b83f8d5591b74e7b4c8c8e6af421739076
goFrendiAsgard <gofrendiasgard@gmail.com>
2024-05-01 11:52:57 +0700

    remove insignificant log

--- src/zrb/__main__.py
+++ src/zrb/__main__.py
@@ -1,10 +1,8 @@
 from zrb.helper.accessories.color import colored
 from zrb.helper.cli import create_cli
 from zrb.helper.default_env import inject_default_env
-from zrb.helper.log import logger
 
 inject_default_env()
-logger.info(colored("Create CLI instance", attrs=["dark"]))
 cli = create_cli()
 
 if __name__ == "__main__":
