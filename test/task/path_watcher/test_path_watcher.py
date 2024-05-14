dc42fd4452ff6228c3c82379b545ffd21796340e
gofrendi <gofrendiasgard@gmail.com>
2024-05-14 15:50:16 +0700

    update test watcher

--- test/task/path_watcher/test_path_watcher.py
+++ test/task/path_watcher/test_path_watcher.py
@@ -15,6 +15,8 @@ def test_path_watcher():
         cwd=os.path.join(_CURRENT_DIR, 'resources'),
         cmd=[
             'touch a/a.txt',
+            'sleep 1',
+            'touch b/b.txt',
         ]
     )
     task = CmdTask(
@@ -22,7 +24,7 @@ def test_path_watcher():
         upstreams=[patch_watcher, toucher],
         cmd='echo {{task.get_xcom("watch-path.file")}}'
     )
-    fn = task.to_function(should_stop_looper=True)
+    fn = task.to_function()
     result = fn()
     output = result.output
-    assert output == os.path.join(_CURRENT_DIR, 'resources', 'a', 'a.txt')
+    assert output == os.path.join(_CURRENT_DIR, 'resources', 'b', 'b.txt')
