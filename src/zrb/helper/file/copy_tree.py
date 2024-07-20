63df36f4acd9f8ef7435eb308400227b1c26b6f0
gofrendi <gofrendiasgard@gmail.com>
2024-07-20 20:16:58 +0700

    ResourceMaker can now handle file

--- src/zrb/helper/file/copy_tree.py
+++ src/zrb/helper/file/copy_tree.py
@@ -26,6 +26,9 @@ async def copy_tree(
         excludes = []
     if skip_parsing is None:
         skip_parsing = []
+    if os.path.isfile(src):
+        await _copy_file(src, dst, replacements, excludes, skip_parsing)
+        return
     names = os.listdir(src)
     new_dst = parse_replacement(dst, replacements)
     if not os.path.exists(new_dst):
@@ -38,13 +41,26 @@ async def copy_tree(
         if os.path.isdir(src_name):
             await copy_tree(src_name, dst_name, replacements, excludes, skip_parsing)
             continue
-        new_dst_name = parse_replacement(dst_name, replacements)
-        shutil.copy2(src_name, new_dst_name)
-        try:
-            if any(fnmatch.fnmatch(new_dst_name, pattern) for pattern in skip_parsing):
-                continue
-            file_content = await read_text_file_async(new_dst_name)
-            new_file_content = parse_replacement(file_content, replacements)
-            await write_text_file_async(new_dst_name, new_file_content)
-        except Exception:
-            logger.error(f"Cannot parse file: {new_dst_name}")
+        await _copy_file(src_name, dst_name, replacements, excludes, skip_parsing)
+
+
+async def _copy_file(
+    src: str,
+    dst: str,
+    replacements: Optional[Mapping[str, str]] = None,
+    excludes: Optional[Iterable[str]] = None,
+    skip_parsing: Optional[Iterable[str]] = None,
+):
+    if any(fnmatch.fnmatch(src, pattern) for pattern in excludes):
+        return
+    new_dst_name = parse_replacement(dst, replacements)
+    if any(fnmatch.fnmatch(new_dst_name, pattern) for pattern in skip_parsing):
+        return
+    if new_dst_name != src:
+        shutil.copy2(src, new_dst_name)
+    try:
+        file_content = await read_text_file_async(new_dst_name)
+        new_file_content = parse_replacement(file_content, replacements)
+        await write_text_file_async(new_dst_name, new_file_content)
+    except Exception:
+        logger.error(f"Cannot parse file: {new_dst_name}")
