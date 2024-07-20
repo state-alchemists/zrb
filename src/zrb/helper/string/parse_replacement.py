5b6208ea0533a19167a61fe45393cc149674a1f0
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2024-07-20 13:37:35 +0700

    Make resource maker aware of indented multiline replacement (#160)
    * Make a multiline aware resource maker
    
    * update setuptools
    
    * add poetry.lock

--- src/zrb/helper/string/parse_replacement.py
+++ src/zrb/helper/string/parse_replacement.py
@@ -1,3 +1,5 @@
+import re
+
 from zrb.helper.accessories.color import colored
 from zrb.helper.log import logger
 from zrb.helper.typecheck import typechecked
@@ -10,5 +12,10 @@ logger.debug(colored("Loading zrb.helper.string.parse_replacment", attrs=["dark"
 def parse_replacement(text: str, replacement: Mapping[str, str]):
     new_text = text
     for old, new in replacement.items():
+        if len(new.strip().split("\n")) > 1:
+            pattern = "".join(["^([ \\t]+)", old])
+            new_replacement = "".join(["\\1", new.replace("\n", "\n\\1")])
+            new_text = re.sub(pattern, new_replacement, new_text, flags=re.MULTILINE)
+            continue
         new_text = new_text.replace(old, new)
     return new_text
