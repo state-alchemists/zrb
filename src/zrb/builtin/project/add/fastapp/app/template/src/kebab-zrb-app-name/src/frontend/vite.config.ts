bb2403e5632f11ffe2f9441c0898676a2de9685e
gofrendi <gofrendiasgard@gmail.com>
2024-08-04 21:50:41 +0700

    fix routing

--- src/zrb/builtin/project/add/fastapp/app/template/src/kebab-zrb-app-name/src/frontend/vite.config.ts
+++ src/zrb/builtin/project/add/fastapp/app/template/src/kebab-zrb-app-name/src/frontend/vite.config.ts
@@ -1,16 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	build: {
-		rollupOptions: {
-			external: [
-				"/static/logo.png"
-			],
-		}
-	}
+	resolve: {
+		alias: {
+			'@static': path.resolve(__dirname, 'static'),
+		},
+	},
 });
