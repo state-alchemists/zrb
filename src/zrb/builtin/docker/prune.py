c10b4bc341bc8e58d6f91747a8a214b7798f32ae
gofrendi <gofrendiasgard@gmail.com>
2024-09-07 14:43:24 +0700

    Better system prune

--- src/zrb/builtin/docker/prune.py
+++ src/zrb/builtin/docker/prune.py
@@ -1,15 +1,21 @@
 from zrb.builtin.docker._group import docker_group
 from zrb.runner import runner
 from zrb.task.cmd_task import CmdTask
+from zrb.task_input.bool_input import BoolInput
 
 prune_docker = CmdTask(
     name="prune",
     group=docker_group,
     description="Prune unused images and volumes",
+    inputs=[
+        BoolInput(
+            name="all", shortcut="a", prompt="Remove all unused images", default=False
+        ),
+        BoolInput(name="volume", shortcut="v", prompt="Clear volume", default=False)
+    ],
     cmd=[
-        "docker system prune -af",
-        "docker image prune -af",
-        "docker system prune -af --volumes",
+        "docker system prune -f {% if input.all %}-a{% endif %} {% if input.volume %}--volumes{% endif %}",  # noqa
+        "docker image prune -f {% if input.all %}-a{% endif %}",
         "docker system df",
     ],
 )
