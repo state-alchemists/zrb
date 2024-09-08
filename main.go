5f4fb50f8a3be28a26eb896c3d3212d1fd4417b0
gofrendi <gofrendiasgard@gmail.com>
2024-09-08 14:45:09 +0700

    Save changes

--- main.go
+++ main.go
@@ -8,17 +8,6 @@ import (
 func main() {
 	name := os.Getenv("NAME")
 	if name == "" {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-		// from repo
-=======
-		// from subrepo
->>>>>>> f0bc2ef0aaef22f25426a88d99447942fcd6755f
->>>>>>> 4412d85d97ab55f906cfca070f93bbe17abe5f27
->>>>>>> 4812fea41c6fc1ae45fb68b9c28459c94728014e
 		name = "World"
 	}
 	fmt.Printf("Hello %s\n", name)
