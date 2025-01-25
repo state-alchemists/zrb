71233ece2d148d5150fdf2899273c162b32c3ddb
goFrendi <gofrendiasgard@gmail.com>
2025-01-25 10:42:38 +0700

    Update task names

--- src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/_zrb/column/add_column_task.py
+++ src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/_zrb/column/add_column_task.py
@@ -1,13 +1,23 @@
 from my_app_name._zrb.format_task import format_my_app_name_code
 from my_app_name._zrb.group import app_create_group
-from my_app_name._zrb.input import new_column_input, new_column_type_input 
+from my_app_name._zrb.input import existing_entity_input, new_column_input, new_column_type_input 
 
 from zrb import AnyContext, make_task, Task
 
 
 @make_task(
+    name="validate-add-my-app-name-column",
+    input=existing_entity_input,
+    retries=0,
+)
+async def validate_add_my_app_name_column(ctx: AnyContext):
+    pass
+
+
+@make_task(
     name="update-my-app-name-schema",
     input=[
+        existing_entity_input,
         new_column_input,
         new_column_type_input,
     ],
