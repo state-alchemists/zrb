548f045697f6fb4c0762cc4a10d36201e8d52731
goFrendi <gofrendiasgard@gmail.com>
2025-02-12 23:02:23 +0700

    Add methods

--- src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/schema/nav.py
+++ src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/schema/nav.py
@@ -1,3 +1,4 @@
+from my_app_name.schema.user import AuthUserResponse
 from pydantic import BaseModel
 
 
@@ -12,6 +13,13 @@ class Menu(BaseModel):
     name: str
     caption: str
     children: list[Submenu] = []
+
+    def get_permitted_children(self, user: AuthUserResponse | None) -> list[Submenu]:
+        if user is None:
+            return []
+        return [
+            child for child in self.children if user.has_permission(child.permission)
+        ]
 
 
 class Nav(BaseModel):
