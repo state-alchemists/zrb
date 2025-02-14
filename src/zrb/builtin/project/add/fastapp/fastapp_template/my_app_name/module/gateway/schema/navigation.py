dbf90d289566ebc3bbf281036d21e670d9cb63f6
gofrendi <gofrendiasgard@gmail.com>
2025-02-14 13:18:34 +0700

    Add navigation

--- src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/schema/navigation.py
+++ src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/schema/navigation.py
@@ -9,16 +9,26 @@ class Submenu(BaseModel):
     permission: str
 
 
+class AccessibleSubmenu(Submenu):
+    active: bool
+
+
 class Menu(BaseModel):
     name: str
     caption: str
     submenus: list[Submenu] = []
 
-    def get_accessible_submenus(self, user: AuthUserResponse | None) -> list[Submenu]:
+    def get_accessible_submenus(
+        self,
+        submenu_name: str | None = None,
+        user: AuthUserResponse | None = None
+    ) -> list[Submenu]:
         if user is None:
             return []
         return [
-            submenu
+            AccessibleSubmenu(
+                **
+            )
             for submenu in self.submenus
             if user.has_permission(submenu.permission)
         ]
@@ -27,9 +37,13 @@ class Menu(BaseModel):
 class Navigation(BaseModel):
     menus: list[Menu] = []
 
-    def get_acccessible_menus(self, user: AuthUserResponse | None) -> list[Menu]:
+    def get_acccessible_menus(
+        self, user: AuthUserResponse | None
+    ) -> list[Menu]:
         if user is None:
             return []
         return [
-            menu for menu in self.menus if len(menu.get_accessible_submenus(user)) > 0
+            menu
+            for menu in self.menus
+            if len(menu.get_accessible_submenus(user)) > 0
         ]
