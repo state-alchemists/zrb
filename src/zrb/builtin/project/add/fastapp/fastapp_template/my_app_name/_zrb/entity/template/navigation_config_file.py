9abbdff243b061093ef53e34291f13b86de0ef27
gofrendi <gofrendiasgard@gmail.com>
2025-02-17 08:38:19 +0700

    Add page nav template

---
+++ src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/_zrb/entity/template/navigation_config_file.py
@@ -0,0 +1,8 @@
+my_module_menu.append_page(
+    Page(
+        name="my-module.my-entity",
+        caption="My Entity",
+        url="/my-module/my-entity",
+        permission="my-entity:read",
+    )
+)
