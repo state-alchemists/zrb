ce62e56ee97aade4eba8af2da15f28a10608f233
gofrendi <gofrendiasgard@gmail.com>
2025-02-18 23:03:26 +0700

    Delay refresh token

--- src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/view/static/common/util.js
+++ src/zrb/builtin/project/add/fastapp/fastapp_template/my_app_name/module/gateway/view/static/common/util.js
@@ -1,32 +1,31 @@
 const UTIL = {
-    refreshAuthToken(refreshSessionIntervalSeconds){
+    async refreshAuthToken(refreshSessionIntervalSeconds){
         const refreshUrl = "/api/v1/user-sessions";
         let shouldRefresh = true;
         async function refresh() {
-            if (!shouldRefresh) {
-                return;
-            }
-            try {
-                const response = await fetch(refreshUrl, {
-                    method: "PUT",
-                    headers: { "Content-Type": "application/json" },
-                    credentials: "include", // Include cookies in the request
-                });
+            while (shouldRefresh) {
+                await new Promise(resolve => setTimeout(resolve, refreshSessionIntervalSeconds * 1000));
+                try {
+                    const response = await fetch(refreshUrl, {
+                        method: "PUT",
+                        headers: { "Content-Type": "application/json" },
+                        credentials: "include", // Include cookies in the request
+                    });
 
-                if (!response.ok) {
-                    if (response.status === 401 || response.status === 403) {
-                        console.warn("Skipping token refresh, authentication required.");
-                        shouldRefresh = false;
-                        return;
+                    if (!response.ok) {
+                        if (response.status === 401 || response.status === 403) {
+                            console.warn("Skipping token refresh, authentication required.");
+                            shouldRefresh = false;
+                            return;
+                        }
+                        throw new Error(`HTTP error! Status: ${response.status}`);
                     }
-                    throw new Error(`HTTP error! Status: ${response.status}`);
+                    console.log("Token refreshed successfully");
+                } catch (error) {
+                    console.error("Cannot refresh token", error)
                 }
-                console.log("Token refreshed successfully");
-            } catch (error) {
-                console.error("Cannot refresh token", error)
             }
         }
-        setInterval(refresh, refreshSessionIntervalSeconds * 1000);
-        refresh();
+        await refresh();
     },
 }
