249dfffdc257e713a12412550efcdc51f21668fd
gofrendi <gofrendiasgard@gmail.com>
2025-03-01 07:46:34 +0700

    Make loading better

--- src/zrb/util/load.py
+++ src/zrb/util/load.py
@@ -12,13 +12,17 @@ pattern = re.compile("[^a-zA-Z0-9]")
 def load_zrb_init(dir_path: str | None = None) -> Any | None:
     if dir_path is None:
         dir_path = os.getcwd()
-    script_path = os.path.join(dir_path, "zrb_init.py")
-    if os.path.isfile(script_path):
-        return load_file(script_path, -1)
-    new_dir_path = os.path.dirname(dir_path)
-    if new_dir_path == dir_path:
-        return
-    return load_zrb_init(new_dir_path)
+    # get path list from current path to the absolute root
+    current_path = os.path.abspath(dir_path)
+    path_list = [current_path]
+    while current_path != os.path.dirname(current_path):  # Stop at root
+        current_path = os.path.dirname(current_path)
+        path_list.append(current_path)
+    # loop from root to current path to load zrb_init
+    for current_path in path_list[::-1]:
+        script_path = os.path.join(current_path, "zrb_init.py")
+        if os.path.isfile(script_path):
+            load_file(script_path)
 
 
 @lru_cache
@@ -29,6 +33,8 @@ def load_file(script_path: str, sys_path_index: int = 0) -> Any | None:
     # Append script dir path
     script_dir_path = os.path.dirname(script_path)
     if script_dir_path not in sys.path:
+        if sys_path_index == -1:
+            sys_path_index = len(sys.path)
         sys.path.insert(sys_path_index, script_dir_path)
     # Add script dir path to Python path
     os.environ["PYTHONPATH"] = _get_new_python_path(script_dir_path)
