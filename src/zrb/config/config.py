b285982c4f733ea40e7f459023d5b669d12227ce
gofrendi <gofrendiasgard@gmail.com>
2025-07-21 23:49:16 +0700

    Add yolo config

--- src/zrb/config/config.py
+++ src/zrb/config/config.py
@@ -288,6 +288,10 @@ class Config:
         return float(os.getenv("ZRB_LLM_THROTTLE_SLEEP", "1.0"))
 
     @property
+    def LLM_YOLO(self) -> bool:
+        return to_boolean(os.getenv("ZRB_LLM_YOLO", "false"))
+
+    @property
     def LLM_SUMMARIZE_HISTORY(self) -> bool:
         return to_boolean(os.getenv("ZRB_LLM_SUMMARIZE_HISTORY", "true"))
 
