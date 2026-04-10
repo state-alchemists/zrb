bd19d060ba4f42fac3a3742f30f0de5da7bce25a
gofrendi <gofrendiasgard@gmail.com>
2026-04-10 21:00:43 +0700

    Update powershell command

--- src/zrb/builtin/shell/autocomplete/powershell.py
+++ src/zrb/builtin/shell/autocomplete/powershell.py
@@ -29,7 +29,7 @@ Register-ArgumentCompleter -Native -CommandName '{command_name}' -ScriptBlock {
     $subcmdOutput = {command_name} shell autocomplete subcmd $cmdArgs 2>$null
 
     if ($subcmdOutput) {
-        $subcmdOutput -split '\s+' | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        $subcmdOutput -split '\\s+' | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
     }
