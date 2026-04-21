3f47a4758b086d13b5a2e1538c9770e6525caa29
Go Frendi <gofrendiasgard@gmail.com>
2026-04-21 22:30:13 +0700

    Use local mermaid

--- src/zrb/runner/web_route/node_page/group/show_group_page.py
+++ src/zrb/runner/web_route/node_page/group/show_group_page.py
@@ -1,55 +1,39 @@
-from pathlib import Path
-
 from zrb.config.config import CFG
 from zrb.group.any_group import AnyGroup
+from zrb.runner.web_route.jinja_env import get_jinja_env
 from zrb.runner.web_schema.user import User
 from zrb.runner.web_util.html import (
     get_html_auth_link,
     get_html_subgroup_info,
     get_html_subtask_info,
 )
-from zrb.util.file import read_file
-from zrb.util.string.format import fstring_format
 
 
 def show_group_page(user: User, root_group: AnyGroup, group: AnyGroup, url: str):
     from fastapi.responses import HTMLResponse
 
-    _DIR = Path(__file__).parent
-    _GLOBAL_TEMPLATE = read_file(
-        str(_DIR.parent.parent / "static" / "global_template.html")
-    )
-    _VIEW_TEMPLATE = read_file(str(_DIR / "view.html"))
     web_title = CFG.WEB_TITLE if CFG.WEB_TITLE.strip() != "" else root_group.name
     web_jargon = (
         CFG.WEB_JARGON if CFG.WEB_JARGON.strip() != "" else root_group.description
     )
     url_parts = url.split("/")
-    parent_url_parts = url_parts[:-2] + [""]
-    parent_url = "/".join(parent_url_parts)
+    parent_url = "/".join(url_parts[:-2] + [""])
     group_info = get_html_subgroup_info(user, url, group)
     task_info = get_html_subtask_info(user, url, group)
     auth_link = get_html_auth_link(user)
-    return HTMLResponse(
-        fstring_format(
-            _GLOBAL_TEMPLATE,
-            {
-                "CFG": CFG,
-                "root_group": root_group,
-                "content": fstring_format(
-                    _VIEW_TEMPLATE,
-                    {
-                        "web_title": web_title,
-                        "web_jargon": web_jargon,
-                        "group_info": group_info,
-                        "task_info": task_info,
-                        "name": group.name,
-                        "description": group.description,
-                        "auth_link": auth_link,
-                        "url": url,
-                        "parent_url": parent_url,
-                    },
-                ),
-            },
+    html = (
+        get_jinja_env()
+        .get_template("node_page/group/view.html")
+        .render(
+            cfg=CFG,
+            web_title=web_title,
+            web_jargon=web_jargon,
+            group_info=group_info,
+            task_info=task_info,
+            name=group.name,
+            description=group.description,
+            auth_link=auth_link,
+            parent_url=parent_url,
         )
     )
+    return HTMLResponse(html)
